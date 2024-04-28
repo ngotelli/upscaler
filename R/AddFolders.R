8334715296d2740777221f5b2433ef97f0336113
ngotelli <ngotelli@uvm.edu>
2024-04-27 19:18:45 -0500

    correct missing title in add folders function

--- R/AddFolders.R
+++ R/AddFolders.R
@@ -5,10 +5,28 @@
 # inputs: optionally, a vector of folder names; default is Nick's folder choices
 # outputs: a set of folders (but not if they already exist)
 ########################################
-add_folder <- function(folder_names=NULL) {
+#' Create Folders
+#'
+#' @param folder_names Character string vector of folder names to create.
+#'  If no folder names are provided, the default folders
+#'  created in the current location (usually a project) are: c("OriginalData",
+#'  "CleanedData","Scripts","Functions","Plots","Outputs","DataObjects",
+#'  "Markdown").Each folder that is created is empty, but does contain a hidden.
+#'  empty file name, so that the empty folders will not be erased by github
+#'  before anything is added to them. The function always checks to make sure
+#'  the folder does not already exist, so it will not over-write existing
+#'  folders or files in the current location.
+#'
+#' @return Set of folders created in the current location.
+#' @export
+#'
+#' @examples
+#' add_folder()
+#' add_folder(folder_names=c("MyFolder","MyFolder2"))
 
 # function body
-  
+
+add_folder <- function(folder_names=NULL) {
 if(is.null(folder_names)) {folder_names <- c("OriginalData",
                   "CleanedData",
                   "Scripts",
@@ -26,12 +44,12 @@ for (i in seq_along(folder_names)) {
                                              " created",sep="",
                                              "\n")
                                        }
-}  
+}
 # add the hidden files
 for (i in seq_along(folder_names)) {
-  if (!file.exists(paste0(folder_names[i],"/.keep"))) file.create(paste0(folder_names[i],"/.keep")) 
-  
-} 
+  if (!file.exists(paste0(folder_names[i],"/.keep"))) file.create(paste0(folder_names[i],"/.keep"))
+
+}
 
 
 } # end of add_folder
